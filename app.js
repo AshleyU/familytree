cbc7c2d9f504491fe5c6e8ef0e44fae0d576b772
Ashley U <ashley.umetsu@gmail.com>
2021-06-28 14:54:28 -0500

    Adding childOf function

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 class familyMember {
-    constructor(name, parents, childOf) {
+    constructor(name, parents) {
         this.name = name;
         this.parents = parents;
-        this.childOf = childOf;
+    }
+
+    childOf() {
+        return this.name + ' is a child of ' + this.parents.join(' & ') + "." || "unknown";
     }
 }
+
+let queenElizabethII = new familyMember("Queen Elizabeth II", ['King George VI', 'Queen Elizabeth']);
+
+console.log(queenElizabethII.childOf());
